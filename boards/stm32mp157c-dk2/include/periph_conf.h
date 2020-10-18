7de7ef6c7144126636e37a92aeda95ee14bd1dbe
Gilles DOFFE <gilles.doffe@savoirfairelinux.com>
2020-10-19 00:21:58 +0200

    fix stm32mp157c-dk2

--- boards/stm32mp157c-dk2/include/periph_conf.h
+++ boards/stm32mp157c-dk2/include/periph_conf.h
@@ -21,64 +21,23 @@
 #ifndef PERIPH_CONF_H
 #define PERIPH_CONF_H
 
+/* This board provides an LSE */
+#ifndef CONFIG_BOARD_HAS_LSE
+#define CONFIG_BOARD_HAS_LSE    1
+#endif
+
+/* This board provides an HSE */
+#ifndef CONFIG_BOARD_HAS_HSE
+#define CONFIG_BOARD_HAS_HSE    1
+#endif
+
 #include "periph_cpu.h"
+#include "mp1/cfg_clock_default_208.h"
 #include "cfg_timer_tim2.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/**
- * @name    Clock settings
- *
- * @note    This is auto-generated from
- *          `cpu/stm32_common/dist/clk_conf/clk_conf.c`
- * @{
- */
-
-/* If not in Engineering mode (Cortex-M4 only), following parameters must be
- * equal to the Cortex-A7 clocks configuration from Linux/U-boot device tree.
- * */
-
-/* give the target core clock (HCLK) frequency [in Hz],
- * maximum: 209MHz (rounded) */
-#define CLOCK_CORECLOCK     MHZ(208)
-/* 0: no external high speed crystal available
- * else: actual crystal frequency [in Hz] */
-#define CLOCK_HSE           MHZ(24)
-/* 0: no external low speed crystal available,
- * 1: external crystal available (always 32.768kHz) */
-#define CLOCK_LSE           (1U)
-/* 0: no internal oscillator
- * else: actual internal oscillator frequency [in Hz] */
-#define CLOCK_HSI           MHZ(64)
-
-/* peripheral clock setup */
-#define CLOCK_MCU           (CLOCK_CORECLOCK / 1)      /* max 209MHz */
-#define CLOCK_APB1          (CLOCK_CORECLOCK / 2)      /* max 104MHz */
-#define CLOCK_APB2          (CLOCK_CORECLOCK / 2)      /* max 104MHz */
-#define CLOCK_APB3          (CLOCK_CORECLOCK / 2)      /* max 104MHz */
-
-/* The following registers should not be set if not in engineering mode
- * (Cortex-M4 only), because it could override Cortex-A7 clock
- * settings.
- */
-#if IS_USED(MODULE_STM32MP1_ENG_MODE)
-
-#define CLOCK_MCU_DIV       RCC_MCUDIVR_MCUDIV_0       /* max 209MHz */
-#define CLOCK_APB1_DIV      RCC_APB1DIVR_APB1DIV_1     /* max 104MHz */
-#define CLOCK_APB2_DIV      RCC_APB2DIVR_APB2DIV_1     /* max 104MHz */
-#define CLOCK_APB3_DIV      RCC_APB3DIVR_APB3DIV_1     /* max 104MHz */
-
-/* Main PLL factors */
-#define CLOCK_PLL_M          (2)
-#define CLOCK_PLL_N          (52)
-#define CLOCK_PLL_P          (3)
-#define CLOCK_PLL_Q          (13)
-
-#endif
-
-/** @} */
 
 /**
  * @name    UART configuration
